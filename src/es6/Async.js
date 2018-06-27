3eb7efd82a01b6f0fa2e253cb80bd8ac691c0d16
fanyonglong <315144604@qq.com>
2018-06-27 08:59:19 +0800

    aa

--- src/es6/Async.js
+++ src/es6/Async.js
@@ -6,3 +6,26 @@ async function getData(src) {
 
         return data;
 }
+function upload(value)
+{
+    return value==2?Promise.reject(value): Promise.resolve(value)
+}
+
+
+async  function test()
+{
+    var result=[];
+    for(var i=0;i<10;i++){
+        // promise 返回失败会抛出异常
+       var o= await upload(i).then(v=>{
+           result.push(v)
+           return v;
+        }).catch(b=>{
+        });
+        console.log(o)
+
+      
+    }
+    console.log(result)
+    return result;
+}
